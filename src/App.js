0cb95592d2b4a1792284b5c14bf9fff0adf2ce8c
Daniela <delciddaniela12@gmail.com>
2022-06-17 15:22:50 -0600

    cambio en react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
 import LoginForm from "./components/LoginFrom";
-=======
 import Aside from "./components/Aside";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
@@ -10,19 +8,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Roles from '../src/components/roles/Roles';
 import Bitacora from "./components/bitacora/Bitacora";
 import Preguntas from "./components/preguntas/Preguntas";
->>>>>>> ce633960de275f3237d8157398ecd291635f8c7b
 
 
 
 
 function App() {
-  return (
-<<<<<<< HEAD
-    <div>
-      <LoginForm/>
-    </div>
-  )
-=======
+ 
     <>
       
       <BrowserRouter>
@@ -32,9 +23,6 @@ function App() {
         </Routes>
       </BrowserRouter>
     </>
-    
-  );
->>>>>>> ce633960de275f3237d8157398ecd291635f8c7b
 }
 
 export default App;
